94bdbde9bff23c471ce3a50180013d87cd2c85b0
Sascha Dörnath <sascha.doernath@gmx.de>
2020-01-12 12:06:01 +0100

    Updates

---
+++ precache-manifest.56ad9102b513e9fd07a2e1ea84a34766.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "59d6f161d5c555af2659f20d6f943e76",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1876eea22dd051c660b0",
+    "url": "/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "4f0c4e9ca95a3a8272a0",
+    "url": "/static/css/main.db70286a.chunk.css"
+  },
+  {
+    "revision": "1876eea22dd051c660b0",
+    "url": "/static/js/2.5a0a1a94.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.5a0a1a94.chunk.js.LICENSE"
+  },
+  {
+    "revision": "4f0c4e9ca95a3a8272a0",
+    "url": "/static/js/main.f591eaa5.chunk.js"
+  },
+  {
+    "revision": "a72cc5513998c854f52e",
+    "url": "/static/js/runtime-main.99854813.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/static/media/SEGA.12a3db8b.woff"
+  }
+]);
