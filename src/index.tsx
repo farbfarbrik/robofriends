b906c73907f8acf0d095c1b834867a03fc3de420
Sascha Dörnath <sascha.doernath@gmx.de>
2020-06-14 22:08:44 +0200

    Cleaned up imports in index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import 'tachyons';
+import { Store } from 'redux';
 import { Provider } from 'react-redux';
 
-import configureStore from './configureStore';
-
 import './index.css';
+import configureStore from './configureStore';
 import App from './containers/App';
-import { Store } from 'redux';
 import { ApplicationState } from './store';
 
 const store: Store<ApplicationState> = configureStore();
